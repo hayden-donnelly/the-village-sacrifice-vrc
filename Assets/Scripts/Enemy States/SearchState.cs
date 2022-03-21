f729ede8e80f8dc17595569f9fea48482b20cb0a
hayden-donnelly <donnellyhd@outlook.com>
2022-03-21 08:48:19 -0400

    Imported assets from original game.

---
+++ Assets/Scripts/Enemy States/SearchState.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchState : BaseState 
+{
+    [SerializeField] private float searchDistance;
+
+    public override void Construct()
+    {
+        Debug.Log("Entering Search State");
+        motor.animator.SetInteger("State", 1);
+        coroutine = Search();
+        StartCoroutine(coroutine);
+    }
+
+    public override void Destruct()
+    {
+        StopCoroutine(coroutine);
+    }
+
+    private IEnumerator Search()
+    {
+        List<Vector3> searchRoute = GenerateSearchRoute(motor.agent.destination);
+        int searchRouteIndex = 0;
+        while(true)
+        {
+            // Follow search route
+            motor.agent.SetDestination(searchRoute[searchRouteIndex]);
+            while(Vector3.Distance(transform.position, motor.agent.destination) > 2)
+            {
+                yield return null;
+            }
+            
+            searchRouteIndex++;
+            if(searchRouteIndex >= searchRoute.Count)
+            {
+                StartCoroutine(ExitToPatrol());
+                break;
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+
+    private IEnumerator ExitToPatrol()
+    {
+        motor.animator.SetInteger("State", 3);  
+        yield return new WaitForSeconds(3);
+        motor.animator.SetInteger("State", 1);
+        motor.ChangeState("PatrolState");  
+    }
+
+    private List<Vector3> GenerateSearchRoute(Vector3 lastSeenPosition)
+    {   
+        // Predict the position that the player will move to
+        Vector3 playerMovement = motor.player.Controller.velocity;
+        playerMovement = new Vector3(playerMovement.x, 0, playerMovement.z);
+        playerMovement.Normalize();
+        playerMovement *= searchDistance;
+        Vector3 predictedPlayerPosition = lastSeenPosition + playerMovement;
+
+        List<Vector3> searchRoute = new List<Vector3>();
+        searchRoute.Add(lastSeenPosition);
+        searchRoute.Add(predictedPlayerPosition);
+        searchRoute.Add(predictedPlayerPosition + (Vector3.right * 10));
+        searchRoute.Add(predictedPlayerPosition + (Vector3.right * -10));
+        searchRoute.Add(predictedPlayerPosition + (Vector3.forward * 10));
+        searchRoute.Add(predictedPlayerPosition + (Vector3.forward * -10));
+
+        return searchRoute;
+    }
+
+    public override void Transition()
+    {
+        if(motor.DetectedPlayer())
+        {
+            motor.ChangeState("ChaseState");
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyAIController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIController : MonoBehaviour 
+{
+	public Animator animator;
+	[SerializeField] private int fieldOfView;
+	[SerializeField] private float sightDetectionDistance;
+	[SerializeField] private float walkingDetectionRadius;
+	[SerializeField] private float crouchingDetectionRadius;
+	[SerializeField] private LayerMask mask;
+	[SerializeField] private Transform head;
+	[HideInInspector] public NavMeshAgent agent;
+	[HideInInspector] public Transform playerTransform;
+	[HideInInspector] public FirstPersonCharacterController player;
+	private BaseState state;
+	private BaseState[] availableStates;
+
+	public enum AnimStates
+	{
+		Walk = 1,
+		Notice = 2,
+		Search = 3,
+	}
+
+	private void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+		GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+		playerTransform = playerObj.transform;
+		player = playerObj.GetComponent<FirstPersonCharacterController>();
+		availableStates = GetComponents<BaseState>();
+		state = GetComponent<PatrolState>();
+		state.Construct();
+	} 
+
+	private void Update()
+	{
+		state.Transition();
+	}
+
+	public void ChangeState(string stateName)
+	{
+		foreach(BaseState s in availableStates)
+		{
+			if(s.GetType().FullName != stateName)
+			{
+				continue;
+			}
+
+			if(s.unlocked)
+			{
+				state.Destruct();
+				state = s;
+				state.Construct();
+			}
+			return;
+		}
+
+		Debug.LogWarning("New state could not be found");
+	}
+
+	public bool DetectedPlayer()
+	{
+		if(!Physics.Linecast(head.position, playerTransform.position, mask))
+		{
+			if(Vector3.Angle(transform.forward, playerTransform.transform.position) < fieldOfView
+				&& Vector3.Distance(transform.position, playerTransform.position) <= sightDetectionDistance)
+			{
+				return true;
+			}
+			if(player.isCrouched)
+			{
+				if(Vector3.Distance(transform.position, playerTransform.position) <= crouchingDetectionRadius)
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if(Vector3.Distance(transform.position, playerTransform.position) <= walkingDetectionRadius)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
