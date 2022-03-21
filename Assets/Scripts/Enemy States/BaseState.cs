469bef65ba39a1a444b4d3dc9bb4984ddbe409ca
hayden-donnelly <hayden.git@proton.me>
2022-03-21 08:48:19 -0400

    Imported assets from original game.

---
+++ Assets/Scripts/Enemy States/BaseState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseState : MonoBehaviour 
+{
+	public bool unlocked;
+	protected IEnumerator coroutine;
+	protected EnemyAIController motor;
+
+	protected void Awake()
+	{
+		motor = GetComponent<EnemyAIController>();
+	}
+
+	public virtual void Construct()
+	{
+
+	}
+
+	public virtual void Destruct()
+	{
+		
+	}
+
+	public virtual void Transition()
+	{
+
+	}
+}
---
+++ Assets/Scripts/GenericUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GenericUI : MonoBehaviour 
+{
+	public void LoadScene(int sceneID)
+	{
+		SceneManager.LoadScene(sceneID);
+	}
+
+	public void ReloadCurrentScene()
+	{
+		SceneManager.LoadScene(GameState.currentSceneID);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
