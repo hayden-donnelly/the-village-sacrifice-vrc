3021a17777fc7bc3496375065bc872fd4589f4a8
hayden-donnelly <donnellyhd@outlook.com>
2022-03-31 11:48:17 -0400

    Created door.

---
+++ Assets/Development/DoorInteraction.cs
@@ -0,0 +1,15 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DoorInteraction : UdonSharpBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(target.position, target.rotation);
+    }
+
+}
