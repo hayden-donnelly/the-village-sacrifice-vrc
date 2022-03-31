89181aba46947f75feced73e2bc0bd1db521892c
hayden-donnelly <hayden.git@proton.me>
2022-03-31 13:24:54 -0400

    Started setting up EnemyController.

---
+++ Assets/Development/BaseState.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BaseState : UdonSharpBehaviour
+{
+    public bool unlocked;
+    protected UdonSharpBehaviour controller;
+
+    protected void Start()
+    {
+        controller = GetComponent<EnemyController>();
+    }
+
+    public virtual void Construct()
+    {
+
+    }
+
+    public virtual void Destruct()
+    {
+
+    }
+
+    public virtual void Transition()
+    {
+
+    }
+}
