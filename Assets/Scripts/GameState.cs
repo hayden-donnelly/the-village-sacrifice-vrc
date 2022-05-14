61ab232c17dfa4a0892042ab0bf9a3ddf973c0a9
hayden-donnelly <donnellyhd@outlook.com>
2022-05-14 18:10:18 -0400

    Deleted old scripts folder

--- Assets/Scripts/GameState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameState 
-{
-	public static bool hasKey = false;
-	public static int currentSceneID = 0;
-}
