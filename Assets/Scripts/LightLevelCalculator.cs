0b31b211efaaa064cdd510d60d753467191191e3
hayden-donnelly <hayden.git@proton.me>
2022-05-26 13:35:49 -0400

    Wrote light calc script -- needs debugging.

---
+++ Assets/Scripts/LightLevelCalculator.cs
@@ -0,0 +1,36 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System.Collections;
+
+public class LightLevelCalculator : UdonSharpBehaviour
+{
+    public float lightLevel;
+    [SerializeField] private RenderTexture lightTexture;
+    [SerializeField] private Texture2D tex;
+
+    void OnPostRender()
+    {
+        //CalculateLightLevel();
+    }
+
+    public float CalculateLightLevel()
+    {
+        Rect rectReadPicture = new Rect(0, 0, 24, 24);
+        //RenderTexture.active = lightTexture;
+        tex.ReadPixels(rectReadPicture, 0, 0, false);
+
+        Vector4 topLeft = tex.GetPixels(0, 0, 1, 1, 0)[0];
+        Vector4 topRight = tex.GetPixels(24, 0, 1, 1, 0)[0];
+        Vector4 bottomLeft = tex.GetPixels(0, 24, 1, 1, 0)[0];
+        Vector4 bottomRight = tex.GetPixels(24, 24, 1, 1, 0)[0];
+
+        float lightLevelSum = topLeft.magnitude + topRight.magnitude 
+                            + bottomLeft.magnitude + bottomRight.magnitude;
+
+
+        Debug.Log(lightLevelSum);
+        return lightLevelSum / 4;
+    }
+}
