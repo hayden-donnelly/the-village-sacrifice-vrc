0a931ade9aa3c37a04ce6c685da11effcc92fb25
hayden-donnelly <hayden.git@proton.me>
2022-06-04 21:07:48 -0400

    Started porting NavMeshComponents to UdonSharp.

---
+++ Assets/Scripts/NavMeshSurf.cs
@@ -0,0 +1,13 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.AI;
+
+public class NavMeshSurf : UdonSharpBehaviour
+{
+    private void Start()
+    {
+        NavMeshBuilder.BuildNavMeshData();
+    }
+}
