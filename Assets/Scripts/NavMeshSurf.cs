d0e2b3de7b76e33b2b8f046a46d1268702d17adc
hayden-donnelly <hayden.git@proton.me>
2022-06-05 15:41:24 -0400

    Ported some NavMesh code to UdonSharp but hit a wall because BuildNavMeshData() takes in a generic list -- a type which UdonSharp currently does not support.

--- Assets/Scripts/NavMeshSurf.cs
+++ Assets/Scripts/NavMeshSurf.cs
@@ -3,11 +3,31 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class NavMeshSurf : UdonSharpBehaviour
 {
+    [SerializeField] private int agentTypeID;
+    [SerializeField] private Mesh mesh;
+
+    public NavMeshBuildSettings GetBuildSettings()
+    {
+        NavMeshBuildSettings buildSettings = NavMesh.GetSettingsByID(agentTypeID);
+        return buildSettings;
+    }
+
+    /*public void BuildNavMesh()
+    {
+        //NavMeshBuildSource[] sources = new NavMeshBuildSource[10];
+        List<NavMeshBuildSource> sources = new List<NavMeshBuildSource>();
+        Bounds sourcesBounds = mesh.bounds;
+
+        NavMeshData data = NavMeshBuilder.BuildNavMeshData(GetBuildSettings(), 
+            sources, sourcesBounds, transform.position, transform.rotation);
+    }*/
+
     private void Start()
     {
-        NavMeshBuilder.BuildNavMeshData();
+        //BuildNavMesh();
     }
 }
